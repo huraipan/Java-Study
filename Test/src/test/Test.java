632a0984810c3f66c4b7e85f8870df34543a596a
huraipan <chki247@naver.com>
2022-12-21 17:16:46 +0900

    chapter2 ch02

---
+++ Test/src/test/Test.java
@@ -0,0 +1,22 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		String s;
+				
+		s = String.valueOf(i); 
+		System.out.println(s);
+		
+		s = Integer.toString(i); 
+		System.out.println(s);
+		
+		s = ""+i;
+		System.out.println(s);
+
+
+	}
+
+}
