f28c8ddfda2c9bb9a16a25faeae4ff25886aa33c
huraipan <chki247@naver.com>
2023-01-04 19:57:46 +0900

    chapter2 ch18

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -4,21 +4,13 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		String s = "10.0";
-		float f;
-		double d;
+		int age = 14;
 		
-		f = Float.parseFloat(s);
-		System.out.println(f); //10
-		
-		d = Double.parseDouble(s);
-		System.out.println(d); //10
-		
-		f = Float.valueOf(s);
-		System.out.println(f); //10
-		
-		d = Double.valueOf(s);
-		System.out.println(d); //10
+		if(age > 7) {
+			System.out.println("초등학생입니다."); // 초등학생입니다.
+		}else if(age > 13){
+			System.out.println("중학생입니다.");
+		}
 
 
 	}
